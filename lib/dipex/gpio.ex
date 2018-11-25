51a7ee670b4768b9efae050430bb1e9b6ebfd965
selfup <boudinot.regis@yahoo.com>
2018-11-24 18:44:12 -0600

    make gpio cmds red

--- lib/dipex/gpio.ex
+++ lib/dipex/gpio.ex
@@ -3,21 +3,21 @@ defmodule Gpio do
 
   def on do
     case System.get_env("FLEX_RPI") do
-      nil -> Logger.warn "nil on"
-      "0" -> Logger.warn "0 on"
+      nil -> Logger.error "nil on"
+      "0" -> Logger.error "0 on"
       "1" ->
+        Logger.error("gpio write 0 1")
         IO.inspect :os.cmd('gpio write 0 1')
-        Logger.warn("gpio write 0 1")
     end
   end
 
   def off do
     case System.get_env("FLEX_RPI") do
-      nil -> Logger.warn "nil off"
-      "0" -> Logger.warn "0 off"
+      nil -> Logger.error "nil off"
+      "0" -> Logger.error "0 off"
       "1" ->
+        Logger.error("gpio write 0 0")
         IO.inspect :os.cmd('gpio write 0 0')
-        Logger.warn("gpio write 0 0")
     end
   end
 
@@ -38,21 +38,21 @@ defmodule Gpio do
 
   def unexport() do
     case System.get_env("FLEX_RPI") do
-      nil -> Logger.warn "nil unexportall"
-      "0" -> Logger.warn "0 unexportall"
+      nil -> Logger.error "nil unexportall"
+      "0" -> Logger.error "0 unexportall"
       "1" ->
+        Logger.error("gpio unexportall")
         IO.inspect :os.cmd('gpio unexportall')
-        Logger.warn("gpio unexportall")
     end
   end
 
   def export() do
     case System.get_env("FLEX_RPI") do
-      nil -> Logger.warn "nil export"
-      "0" -> Logger.warn "0 export"
+      nil -> Logger.error "nil export"
+      "0" -> Logger.error "0 export"
       "1" ->
+        Logger.error("gpio mode 0 out")
         IO.inspect :os.cmd('gpio mode 0 out')
-        Logger.warn("gpio mode 0 out")
     end
   end
 end
